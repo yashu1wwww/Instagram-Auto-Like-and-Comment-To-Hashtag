b70bf797a48998641d6cb09d62c283fc077a8e58
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-09-18 12:30:59 +0530

    Update hlc.py

--- hlc.py
+++ hlc.py
@@ -8,7 +8,7 @@ from time import sleep
 from selenium.webdriver import ActionChains
 import random
 
-hashtag = 'thaaladhoni' #replace with your hashtag
+hashtag = 'instagram' #replace with your hashtag
 
 commentsDict = ['good','amazing one','keep going','excellent','next video please','follow your page','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
 'what a video man','watched yesterday','your are genious','faster than light','your work needed success','new fan of you','keep rock dude','copy cat','link the video','listening','writing','reading','playing',] 
@@ -16,13 +16,14 @@ commentsDict = ['good','amazing one','keep going','excellent','next video please
 driver = webdriver.Chrome()
 driver.get("https://www.instagram.com")
 sleep(3)
-driver.find_element_by_name('username').send_keys('yashu_ini') #replace with your insta username
-driver.find_element_by_name('password').send_keys('pas32%') #replace with your insta password
+driver.find_element_by_name('username').send_keys('instagram') #replace with your insta username
 sleep(1)
+driver.find_element_by_name('password').send_keys('insta_123@#$%') #replace with your insta password
+sleep(2)
 driver.find_element_by_xpath("//button[@type='submit']").click()
 sleep(6)
 driver.get(f'https://www.instagram.com/explore/tags/{hashtag}/') #hashtag
-sleep(10)
+sleep(8)
 post_click=driver.find_element_by_class_name("_aagw").click() #click on first post 
 sleep(3)
 like=driver.find_element_by_class_name("_aamw").click() #click on like button
@@ -31,10 +32,10 @@ driver.find_element(By.XPATH,'//*[@class ="_akhn"]//textarea').click() #click on
 sleep(1)
 driver.find_element(By.XPATH,'//*[@class ="_akhn"]//textarea').send_keys(random.choice(commentsDict)) #send the text in comment section
 sleep(1)
-driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]/div').click() #click on post button of comment
+driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]').click() #click on post button of comment
 sleep(3)
-next_button=driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div/button').click()
-sleep(5)
+next_button=driver.find_element_by_class_name("_abl-").click() #next button
+sleep(4)
 
 while True:
     try:
@@ -56,20 +57,17 @@ while True:
         sleep(1)
         
         # Post the comment
-        post_button = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]/div')
+        post_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]')
         post_button.click()
+        
+        sleep(1)
 
         # Click the next post button
-        next_button = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button')
+        next_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]')
         next_button.click()
 
         sleep(3)  
 
     except Exception as e:
         print("An error occurred:", e)
-        break  # Exit the loop if an error occurs
-
-
-
-
-
+        break  
