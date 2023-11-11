69bc89715c995a3b337d9eff192cc1f6aaf8c1cb
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-11-11 14:21:48 +0530

    Update hlc.py

--- hlc.py
+++ hlc.py
@@ -16,9 +16,9 @@ commentsDict = ['good','amazing one','keep going','excellent','next video please
 driver = webdriver.Chrome()
 driver.get("https://www.instagram.com")
 sleep(3)
-driver.find_element_by_name('username').send_keys('instagram') #replace with your insta username
+driver.find_element_by_name('username').send_keys('insta123') #replace with your insta username
 sleep(1)
-driver.find_element_by_name('password').send_keys('insta_123@#$%') #replace with your insta password
+driver.find_element_by_name('password').send_keys('insta123@#$%') #replace with your insta password
 sleep(2)
 driver.find_element_by_xpath("//button[@type='submit']").click()
 sleep(6)
@@ -32,9 +32,9 @@ driver.find_element(By.XPATH,'//*[@class ="_akhn"]//textarea').click() #click on
 sleep(1)
 driver.find_element(By.XPATH,'//*[@class ="_akhn"]//textarea').send_keys(random.choice(commentsDict)) #send the text in comment section
 sleep(1)
-driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]').click() #click on post button of comment
+driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x1qjc9v5.xjbqb8w.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xr1yuqi.xkrivgy.x4ii5y1.x1gryazu.x15h9jz8.x47corl.xh8yej3.xir0mxb.x1juhsu6 > div > article > div > div._ae65 > div > div > div._ae2s._ae3v._ae3w > section._aaoe._ae5y._ae5z._ae62 > div > form > div > div._am-5 > div").click()') #click on post button of comment....
 sleep(3)
-next_button=driver.find_element_by_class_name("_abl-").click() #next button
+next_button=driver.find_element_by_class_name("_abl-").click() #next button (code changed)
 sleep(4)
 
 while True:
@@ -54,20 +54,23 @@ while True:
         comment_text = driver.find_element(By.XPATH, '//*[@class ="_akhn"]//textarea')
         comment_text.send_keys(random.choice(commentsDict))
 
-        sleep(1)
+        sleep(2)
         
-        # Post the comment
-        post_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]')
-        post_button.click()
+        #send a comment...
+        driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x1qjc9v5.xjbqb8w.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xr1yuqi.xkrivgy.x4ii5y1.x1gryazu.x15h9jz8.x47corl.xh8yej3.xir0mxb.x1juhsu6 > div > article > div > div._ae65 > div > div > div._ae2s._ae3v._ae3w > section._aaoe._ae5y._ae5z._ae62 > div > form > div > div._am-5 > div").click()') 
         
         sleep(1)
 
-        # Click the next post button
-        next_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]')
-        next_button.click()
+        # Click the next post button ....
+        driver.execute_script('document.querySelector("body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div:nth-child(1) > div > div > div._aaqg._aaqh > button").click()')
 
         sleep(3)  
 
     except Exception as e:
         print("An error occurred:", e)
         break  
+        
+        
+
+ 
+
